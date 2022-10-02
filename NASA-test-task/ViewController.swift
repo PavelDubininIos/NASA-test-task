6549ef8fafb3906608d62813cd4c1a6f0f882430
Pavel Dubinin <pavel.ois.develop@gmail.com>
2022-10-02 19:34:11 +0300

    Initial Commit

---
+++ NASA-test-task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NASA-test-task
+//
+//  Created by Павел Дубинин on 02.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
